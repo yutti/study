943c60ed03414e397363508b26cf5e8751b24a4e
yutaka <1ban.2ban.urban@gmail.com>
2022-06-16 22:50:15 +0900

    Chagne effect botton to combobox

--- img_effector/run_gui.py
+++ img_effector/run_gui.py
@@ -14,8 +14,8 @@ class Set_gui():
          
         # Variable setting
         self.file_filter = [("Image file", ".bmp .png .jpg .tif")]
-        #function_btn =["botton_A","botton_B","botton_C","botton_D","botton_E","botton_F"]        
-        canvas_title =["Base","Effect"]
+        self.function_btn =["Gray_scale","Binarization","Sepia"]        
+        self.canvas_title =["Base","Effect"]
         
         # Main window
         self.main_window = main_window
@@ -43,34 +43,20 @@ class Set_gui():
         self.imp_path_box.grid(row=1, column=0, sticky=tk.EW, padx=10)
   
         # 2. Set Function botton
-
-        self.button = tk.Button(self.func_frame, text = "Gray_scale", width=10, command=self.convert_gray_scale)
-        self.button.grid(row=0, column=0, pady = 20)        
-
-        self.button = tk.Button(self.func_frame, text = "Binarization", width=10, command=self.convert_binarization)
-        self.button.grid(row=1, column=0, pady = 20)  
+        self.func_combobox = ttk.Combobox(self.func_frame ,text = 'combo_file', state="readonly", value=self.function_btn,
+        width=30)
+        self.func_combobox.set(self.function_btn[0])
+        self.func_combobox.bind("<<ComboboxSelected>>", self.effect_event)
+        self.func_combobox.pack()
         
-        self.button = tk.Button(self.func_frame, text = "Sepia", width=10, command=self.convert_sepia)
-        self.button.grid(row=2, column=0, pady = 20)  
-                
-        #for i in range(6):
-        #    self.button = tk.Button(self.func_frame, text = function_btn[i], width=10, command=self.on_click_close)
-        #    self.button.grid(row=i, column=0, pady = 20)        
-
         # 3. Set canvas_frame
         
         # 3.1 canvas_frame title
         for i in range(2):
-            label = tk.Label(self.canvas_frame, text=canvas_title[i], bg="white", relief=tk.RIDGE)
+            label = tk.Label(self.canvas_frame, text=self.canvas_title[i], bg="white", relief=tk.RIDGE)
             label.grid(row=0, column=i,sticky=tk.W + tk.E)               
 
         # 3.2 canvas_frame title
-        
-        # tk
-        # self.image_tk  = tk.PhotoImage(file="lenna.png")
-        #for i in range(2):
-        #    label = tk.Label(self.canvas_frame, image=self.image_tk)
-        #    label.grid(row=1, column=i)  
         # from PIL import ImageTk
         self.base_img   = Image.open('lenna.png')  # POR image 
         self.effect_img = self.base_img            # Effect image
@@ -81,9 +67,6 @@ class Set_gui():
         self.base_img_label = tk.Label(self.canvas_frame, image=self.pil_base_img)
         self.base_img_label.grid(row=1, column=0)
 
-        #self.effect_img_label = tk.Canvas(self.canvas_frame, image=self.pil_effect_img)
-        #self.effect_img_label.grid(row=1, column=1)
-
         # canvas
         self.effect_img_canvas = tk.Canvas(self.canvas_frame, width=self.effect_img.width, height=self.effect_img.height)
         self.effect_img_canvas.grid(row=1, column=1)
@@ -91,7 +74,6 @@ class Set_gui():
         # canvasに初期画像を表示
         self.effect_img_canvas.photo = ImageTk.PhotoImage(self.effect_img)
         self.effect_img_on_canvas = self.effect_img_canvas.create_image(0, 0, anchor='nw', image=self.effect_img_canvas.photo)
-        
         
         # 4 set to close the window
         self.button = tk.Button(self.exit_frame, text='Exit', width=10, command=self.on_click_close)
@@ -106,28 +88,33 @@ class Set_gui():
         if self.filename:
             self.img_path.set(self.filename)
         img_file = self.control.file_set(self.filename)
+
+    def effect_event(self, arg):
+        #print(self.func_combobox.get(), arg)
+        #print(self.func_combobox.current(), self.func_combobox.get())
         
-    def convert_gray_scale(self):
-        self.convert_img = self.effect_img.convert('L')
-        self.set_image(self.convert_img)
+        # gary scale
+        if self.func_combobox.current() <= 3:
+            self.convert_img = self.effect_img.convert('L')
+        
+            if self.func_combobox.get() == "Gray_scale":
+                self.set_image(self.convert_img)            
 
-    def convert_binarization(self):
-        self.convert_img = self.effect_img.convert('L')
-        self.converted_img = self.convert_img.point(lambda x: 0 if x < 230 else x) 
-        self.set_image(self.converted_img)
+            elif self.func_combobox.get() == "Binarization":
+                self.converted_img = self.convert_img.point(lambda x: 0 if x < 230 else x) 
+                self.set_image(self.converted_img)
 
-    def convert_sepia(self):
-        self.convert_img = self.effect_img.convert('L')
-        self.merged_img = Image.merge(
-            "RGB",
-            (      
-                self.convert_img.point(lambda x: x * 240 / 255),
-                self.convert_img.point(lambda x: x * 200 / 255),
-                self.convert_img.point(lambda x: x * 145 / 255)
-            )
-        )
-        self.set_image(self.merged_img)
-
+            elif self.func_combobox.get() == "Sepia":
+                self.merged_img = Image.merge(
+                    "RGB",
+                    (      
+                        self.convert_img.point(lambda x: x * 240 / 255),
+                        self.convert_img.point(lambda x: x * 200 / 255),
+                        self.convert_img.point(lambda x: x * 145 / 255)
+                    )
+                )
+                self.set_image(self.merged_img)
+  
     def set_image(self, pic_img):
         self.effect_img_canvas.photo = ImageTk.PhotoImage(pic_img)
         self.effect_img_canvas.itemconfig(self.effect_img_on_canvas, image=self.effect_img_canvas.photo)
