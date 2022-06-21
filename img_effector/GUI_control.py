99e7bd01c97e58fbea7d4b85b6666d4fd06f26db
yutaka <1ban.2ban.urban@gmail.com>
2022-06-21 16:51:39 +0900

    Run black and isort

--- img_effector/GUI_control.py
+++ img_effector/GUI_control.py
@@ -1,30 +1,30 @@
 from GUI_model import GUI_model
 
-class GUI_control():
 
+class GUI_control:
     def __init__(self):
         self.model = GUI_model()
 
     # Function for closing window
-        
+
     def get_image_path(self):
         img_file_path = self.model.get_path()
         return img_file_path
 
-    def draw_image(self, im_path,base_canvas,effect_canvas):
-        resize_img, base_img_canvas, effect_canvas_create = self.model.resize_image(im_path,base_canvas,effect_canvas)
+    def draw_image(self, im_path, base_canvas, effect_canvas):
+        resize_img, base_img_canvas, effect_canvas_create = self.model.resize_image(
+            im_path, base_canvas, effect_canvas
+        )
         return resize_img, base_img_canvas, effect_canvas_create
-        
 
-   
     def effect_image(self, effect_img, func_combobox):
 
-        func_no   = func_combobox.current()
-        func_name = func_combobox.get() 
+        func_no = func_combobox.current()
+        func_name = func_combobox.get()
 
         if func_no <= 2:
             convert_img = self.model.retouch_gray_scale(effect_img)
-        
+
             if func_name == "Gray_scale":
                 converted_img = convert_img
 
@@ -36,40 +36,38 @@ class GUI_control():
 
         elif func_name == "Jagged_mosaic":
             converted_img = self.model.retouch_jagged_mosaic(effect_img)
-            
+
         elif func_name == "Soft_mosaic":
             converted_img = self.model.retouch_soft_mosaic(effect_img)
 
         elif func_name == "Quantize":
             converted_img = self.model.retouch_alpha_blend(effect_img)
-            
+
         elif func_name == "Invert":
-            converted_img = self.model.retouch_invert(effect_img)        
+            converted_img = self.model.retouch_invert(effect_img)
 
         elif func_name == "Mirror":
-            converted_img = self.model.retouch_mirror(effect_img) 
+            converted_img = self.model.retouch_mirror(effect_img)
 
         elif func_name == "Flip":
-            converted_img = self.model.retouch_flip(effect_img) 
+            converted_img = self.model.retouch_flip(effect_img)
 
         elif func_name == "Posterize":
-            converted_img = self.model.retouch_posterize(effect_img) 
+            converted_img = self.model.retouch_posterize(effect_img)
 
         elif func_name == "Solarize":
-            converted_img = self.model.retouch_solarize(effect_img) 
+            converted_img = self.model.retouch_solarize(effect_img)
 
         elif func_name == "Equalize":
-            converted_img = self.model.retouch_equalize(effect_img) 
- 
+            converted_img = self.model.retouch_equalize(effect_img)
+
         elif func_name == "Counter":
-            converted_img = self.model.retouch_counter(effect_img) 
- 
+            converted_img = self.model.retouch_counter(effect_img)
+
         elif func_name == "Emboss":
-            converted_img = self.model.retouch_emboss(effect_img) 
- 
+            converted_img = self.model.retouch_emboss(effect_img)
+
         elif func_name == "Find_emboss":
-            converted_img = self.model.retouch_findemboss(effect_img) 
-            
-              
+            converted_img = self.model.retouch_findemboss(effect_img)
+
         return converted_img
-  
--- img_effector/run_gui.py
+++ img_effector/run_gui.py
@@ -1,97 +1,145 @@
 import tkinter as tk
 from tkinter import ttk
+
+from PIL import Image, ImageOps, ImageTk
+
 from GUI_control import GUI_control
-from PIL import Image, ImageTk, ImageOps
 
-class Set_gui():
 
+class Set_gui:
     def __init__(self, main_window):
 
         # controller Class生成
         self.control = GUI_control()
-         
+
         # Variable setting
-        self.function_btn =["Gray_scale","Binarization","Sepia","Jagged_mosaic","Soft_mosaic","Quantize",
-                            "Invert","Mirror","Flip","Posterize","Solarize","Equalize","Counter","Emboss",
-                            "Find_emboss"]        
-        self.canvas_title =["Base","Effect"]
-        
+        self.function_btn = [
+            "Gray_scale",
+            "Binarization",
+            "Sepia",
+            "Jagged_mosaic",
+            "Soft_mosaic",
+            "Quantize",
+            "Invert",
+            "Mirror",
+            "Flip",
+            "Posterize",
+            "Solarize",
+            "Equalize",
+            "Counter",
+            "Emboss",
+            "Find_emboss",
+        ]
+        self.canvas_title = ["Base", "Effect"]
+
         # Main window
         self.main_window = main_window
         self.main_window.geometry("1400x800")
-        self.main_window.title('Image Editor v0.10')    
-    
+        self.main_window.title("Image Editor v0.10")
+
         # Sub window
         self.filepath_frame = tk.Frame(self.main_window, height=100, width=400)
-        self.func_frame     = tk.Frame(self.main_window, height=450, width=400)
-        self.exit_frame     = tk.Frame(self.main_window, height=100, width=400)
-        self.canvas_frame   = tk.Frame(self.main_window, height=450, width=400)       
+        self.func_frame = tk.Frame(self.main_window, height=450, width=400)
+        self.exit_frame = tk.Frame(self.main_window, height=100, width=400)
+        self.canvas_frame = tk.Frame(self.main_window, height=450, width=400)
 
         # Widgetsmith
         self.filepath_frame.place(relx=0.05, rely=0.8)
         self.func_frame.place(relx=0.85, rely=0.15)
-        self.exit_frame.place(relx=0.85, rely=0.8)        
-        self.canvas_frame.place(relx=0.05, rely=0.05)          
- 
+        self.exit_frame.place(relx=0.85, rely=0.8)
+        self.canvas_frame.place(relx=0.05, rely=0.05)
+
         # 1. Set file path
-        self.imp_path_btn   = tk.Button(self.filepath_frame, text="Select_image_file", command=self.draw_default_image)
-        self.imp_path_btn.grid(row=0, column=0, sticky=tk.W , padx=10, pady = 5)
+        self.imp_path_btn = tk.Button(
+            self.filepath_frame,
+            text="Select_image_file",
+            command=self.draw_default_image,
+        )
+        self.imp_path_btn.grid(row=0, column=0, sticky=tk.W, padx=10, pady=5)
 
         self.img_path_stvar = tk.StringVar()
-        self.imp_path_entry = tk.Entry(self.filepath_frame, textvariable=self.img_path_stvar, width=70)
+        self.imp_path_entry = tk.Entry(
+            self.filepath_frame, textvariable=self.img_path_stvar, width=70
+        )
         self.imp_path_entry.grid(row=1, column=0, sticky=tk.EW, padx=10)
-  
+
         # 2. Set Function botton
-        self.func_combobox = ttk.Combobox(self.func_frame ,text = 'combo_file', state="readonly", value=self.function_btn,
-        width=30)
+        self.func_combobox = ttk.Combobox(
+            self.func_frame,
+            text="combo_file",
+            state="readonly",
+            value=self.function_btn,
+            width=30,
+        )
         self.func_combobox.set(self.function_btn[0])
         self.func_combobox.bind("<<ComboboxSelected>>", self.effect_event)
         self.func_combobox.pack()
-        
+
         # 3.1 canvas_frame title
         for i in range(2):
-            label = tk.Label(self.canvas_frame, text=self.canvas_title[i], bg="white", relief=tk.RIDGE)
-            label.grid(row=0, column=i,sticky=tk.W + tk.E)               
+            label = tk.Label(
+                self.canvas_frame,
+                text=self.canvas_title[i],
+                bg="white",
+                relief=tk.RIDGE,
+            )
+            label.grid(row=0, column=i, sticky=tk.W + tk.E)
 
         # 3.2 canvas_frame title
-        self.base_canvas   = tk.Canvas(self.canvas_frame, width=500, height=500, bg = "#A9A9A9")
+        self.base_canvas = tk.Canvas(
+            self.canvas_frame, width=500, height=500, bg="#A9A9A9"
+        )
         self.base_canvas.grid(row=1, column=0)
-        
-        self.effect_canvas = tk.Canvas(self.canvas_frame, width=500, height=500, bg = "#A9A9A9")
+
+        self.effect_canvas = tk.Canvas(
+            self.canvas_frame, width=500, height=500, bg="#A9A9A9"
+        )
         self.effect_canvas.grid(row=1, column=1)
-       
+
         # 4 set to close the window
-        self.button = tk.Button(self.exit_frame, text='Exit', width=10, command=self.on_click_close)
-        self.button.grid(row=0, column=0, sticky=tk.SE, padx = 10, pady = 10)
+        self.button = tk.Button(
+            self.exit_frame, text="Exit", width=10, command=self.on_click_close
+        )
+        self.button.grid(row=0, column=0, sticky=tk.SE, padx=10, pady=10)
 
     # Function for closing window
     def on_click_close(self):
         self.main_window.destroy()
-        
+
     def draw_default_image(self):
-        self.img_path   = self.control.get_image_path()
+        self.img_path = self.control.get_image_path()
         self.img_path_stvar.set(self.img_path)
 
-        self.effect_img , self.base_canvas, self.effect_canvas_create = self.control.draw_image(self.img_path,self.base_canvas,self.effect_canvas)
+        (
+            self.effect_img,
+            self.base_canvas,
+            self.effect_canvas_create,
+        ) = self.control.draw_image(self.img_path, self.base_canvas, self.effect_canvas)
 
     def effect_event(self, arg):
-        self.converted_img = self.control.effect_image(self.effect_img, self.func_combobox)
+        self.converted_img = self.control.effect_image(
+            self.effect_img, self.func_combobox
+        )
         self.replace_effect_image(self.converted_img)
-  
+
     def replace_effect_image(self, pic_img):
         self.effect_canvas.photo = ImageTk.PhotoImage(pic_img)
-        self.effect_canvas.itemconfig(self.effect_canvas_create, image=self.effect_canvas.photo)
+        self.effect_canvas.itemconfig(
+            self.effect_canvas_create, image=self.effect_canvas.photo
+        )
+
 
 def main():
 
-    #　Tk MainWindow
+    # 　Tk MainWindow
     main_window = tk.Tk()
-    
+
     # Viewクラス生成
     Set_gui(main_window)
-    
-    #　フレームループ処理
+
+    # 　フレームループ処理
     main_window.mainloop()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
